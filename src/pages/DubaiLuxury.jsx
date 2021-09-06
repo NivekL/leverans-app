1171ee58176fb517a1e7692986038114756c3c7d
brorminskij <alexander.borminskij@gmail.com>
2021-09-06 14:32:51 +0200

    Card, category pages, clean up App.jsx/logo.svg
    Created Card component, added three different categories (pages) and cleaned up in App.jsx, removed logo.svg.

---
+++ src/pages/DubaiLuxury.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Card } from '../components/Card'
+
+export const DubaiLuxury = () => {
+    return (
+        <div>
+            <Card />
+        </div>
+    )
+}
