09c2e5656866ddc4042cad52da506221aa6e395d
brorminskij <alexander.borminskij@gmail.com>
2021-09-06 15:47:41 +0200

    added category pages, card info for testing static

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,24 +1,40 @@
 import React from "react";
 import testImg from "../images/Dubai_Luxury/Audemars_Piguet-Royal_Oak_Concept.jpg";
+import styled from "styled-components";
+
+const White = styled.div`
+  color: black;
+`
+const Font12 = styled.div`
+font-size: 12px;
+`
+const Price = styled.div`
+font-size: 14px;
+font-style: oblique;
+`
+
+
 
 export const Card = () => {
   return (
-    <div style={{ color: "#000000"}}>
+    <White>
       <img
         style={{ maxWidth: "100%" }}
         src={testImg}
         alt="Audemar Piguet watch"
         height="300px"
       />
-      <div>
+      <Font12>
         <div>
-          <strong style={{fontSize: '12px'}}>SUB Ocean Plastic - Deep Blue</strong>
+          <strong>SUB Ocean Plastic - Deep Blue</strong>
         </div>
-        <div style={{fontSize: '12px'}}>Ocean Deep Blue</div>
-      </div>
-      <div>
-        <span style={{fontSize: '14px', fontStyle: 'oblique'}}>1395.00 SEK</span>
-      </div>
-    </div>
+        <div>Ocean Deep Blue</div>
+      </Font12>
+      <Price>
+        <span>1395.00 SEK</span>
+      </Price>
+      
+      
+    </White>
   );
 };
--- src/pages/DubaiLuxury.jsx
+++ src/pages/DubaiLuxury.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import { Card } from '../components/Card'
+import styled from 'styled-components'
+
+const Lux = styled.div`
+margin-left: 20px;
+font-style: normal;
+font-weight: 400;
+font-size: 15px;
+line-height: 18px;
+max-width: 40px;
+`
 
 export const DubaiLuxury = () => {
     return (
         <div>
+            <Lux>Time for Luxury</Lux>
             <Card />
         </div>
     )
