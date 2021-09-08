5f46529ccbe4988d2ca3021f4a9bde20d973839e
oscarlundberg-sthlm <oscarlundberg@hotmail.com>
2021-09-08 10:02:24 +0200

    added small errorhandling in backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
-app.use(express.static(path.join(__dirname, '../build')));
+
+const buildDir = path.join(__dirname, '../build');
+
+if (fs.existsSync(buildDir)) {
+    app.use(express.static(path.join(__dirname, '../build')));
+} else {
+    console.log("Can't serve an empty folder. No build folder exists. Remember to run the build script =)");
+}
 
 const port = '4000';
 
