8ca82f03fd2a84d2eeb893a179a56f1306a8bab3
brorminskij <alexander.borminskij@gmail.com>
2021-09-10 14:18:52 +0200

    removed styling, import styling component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Link} from "react-router-dom";
+import { Watchcon } from '../pages/category-styling';
 
 
 export const Card = ({ cards }) => {
@@ -28,45 +29,3 @@ export const Card = ({ cards }) => {
       </Watchcon>
     );
   }
-
-  
-// STYLING
-const Watchcon = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  flex-wrap: wrap;
-
-  .card {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    text-align: center;
-    background-color: whitesmoke;
-    margin-bottom: 20px;
-    padding-bottom: 10px;
-    font-family: "Libre Franklin", sans-serif;
-    cursor: pointer;
-    text-decoration: none;
-    color: #000;
-  }
-
-  p:last-of-type {
-    font-size: 12px;
-    padding-top: 5px;
-    font-weight: bold;
-  }
-
-  img {
-    height: 180px;
-    @media screen and (min-width: 1024px) {
-      height: 480px;
-    }
-    @media screen and (min-width: 1200px) {
-      height: 480px;
-    }
-    @media screen and (max-width: 481px) {
-      height: auto;
-      width: 100%;
-    }
-  }`
