f698045fefcd9c30b057a3d16eaca85eded3f522
Igor Ljevak <igorljevak@gmail.com>
2021-09-13 13:51:19 +0200

    Toggle signup Login and user page

--- src/components/DownNav.jsx
+++ src/components/DownNav.jsx
@@ -1,126 +1,120 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import { Link } from "react-router-dom";
-import Login from './Login'
-import SignUp from './SignUp'
-
-
-
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import UserProfile from './UserProfile';
+import LoginSignUp from './LoginSignUp';
 
 function DownNav({ open, setOpen }) {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
 
-    const [toggleLogIn, setToggleLogIn] = useState(true)
-
-
-    return ( 
-        <MenuWrapper open={open}>
-            <Ul >
-            <h3>Kategorier</h3>
-                <li><MenuLink to="/LondonClassic" onClick={() => setOpen(!open)}>Classic</MenuLink></li>
-                <li><MenuLink to="/StMoritzSport" onClick={() => setOpen(!open)}>Sport</MenuLink> </li>
-                <li><MenuLink to="/DubaiLuxury" onClick={() => setOpen(!open)}>Luxury</MenuLink></li>
-                <Link to="/" className="showAllLink" onClick={() => setOpen(!open)}>visa alla </Link>
-            </Ul>
-            <UserContainer>
-                {toggleLogIn ?  
-            <div>   
-                <h3>hej, logga in</h3>    
-                <Login toggleLogIn={toggleLogIn} setToggleLogIn={setToggleLogIn}/>
-            </div>
-                :
-            <div>
-                <h3>hej, registrera dig</h3> 
-                <SignUp toggleLogIn={toggleLogIn} setToggleLogIn={setToggleLogIn}/>
-            </div> 
-            }
-           
-            
-            </UserContainer>
-        </MenuWrapper>
-    )
+  return (
+    <MenuWrapper open={open}>
+      <Ul>
+        <h3>Kategorier</h3>
+        <li>
+          <MenuLink to="/LondonClassic" onClick={() => setOpen(!open)}>
+            Classic
+          </MenuLink>
+        </li>
+        <li>
+          <MenuLink to="/StMoritzSport" onClick={() => setOpen(!open)}>
+            Sport
+          </MenuLink>{' '}
+        </li>
+        <li>
+          <MenuLink to="/DubaiLuxury" onClick={() => setOpen(!open)}>
+            Luxury
+          </MenuLink>
+        </li>
+        <Link to="/" className="showAllLink" onClick={() => setOpen(!open)}>
+          visa alla{' '}
+        </Link>
+      </Ul>
+      <UserContainer>
+        {isLoggedIn ? (
+          <UserProfile isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+        ) : (
+          <LoginSignUp isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+        )}
+      </UserContainer>
+    </MenuWrapper>
+  );
 }
 
-export default DownNav
-
+export default DownNav;
 
 // Styled ----------------------------------------------------------------------
 const MenuWrapper = styled.div`
-display: flex;
-justify-content: space-between;
-position: absolute;
-top: 4rem;
-left: 0;
-height: 17.4rem;
-width: 100%;
-padding-top: 2rem;
-padding-left: 1rem;
-background-color: whitesmoke;
-color: #292929;
-font-family: 'Libre Franklin', sans-serif;
-transform: ${({ open }) => open ? 'translatex(0)' : 'translateY(-140%)'};
-transition: transform 0.3s ease-in-out;
+  display: flex;
+  justify-content: space-between;
+  position: absolute;
+  top: 4rem;
+  left: 0;
+  height: 17.4rem;
+  width: 100%;
+  padding-top: 2rem;
+  padding-left: 1rem;
+  background-color: whitesmoke;
+  color: #292929;
+  font-family: 'Libre Franklin', sans-serif;
+  transform: ${({ open }) => (open ? 'translatex(0)' : 'translateY(-140%)')};
+  transition: transform 0.3s ease-in-out;
 
-    h3{
+  h3 {
     margin-bottom: 1.2rem;
-    font-size: .8rem;
+    font-size: 0.8rem;
     font-weight: 200;
     letter-spacing: 2px;
     text-transform: uppercase;
-    }
-`
+  }
+`;
 
 const MenuLink = styled(Link)`
-text-decoration: none;
-font-size: 1.5rem;
-color: #292929;
+  text-decoration: none;
+  font-size: 1.5rem;
+  color: #292929;
 
-
-&:hover{
+  &:hover {
     border-bottom: 3px solid #292929;
-}
-`
-
+  }
+`;
 
 const Ul = styled.ul`
-    list-style: none;
-    display: flex;
-    flex-flow: row nowrap;
-    flex-flow: column nowrap;
-    
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
+  flex-flow: column nowrap;
 
-  
-    li { 
-        padding-bottom: 1rem;
+  li {
+    padding-bottom: 1rem;
+  }
+
+  .showAllLink {
+    text-decoration: none;
+    font-size: 0.8rem;
+    margin-top: 1.4rem;
+    color: #292929;
+    letter-spacing: 1px;
+    width: 5rem;
+    line-height: -20px;
+    text-transform: uppercase;
+
+    &:hover {
+      border-bottom: 2px solid #292929;
     }
-    
-    .showAllLink{
-        text-decoration: none;
-        font-size: .8rem;
-        margin-top: 1.4rem;
-        color: #292929;
-        letter-spacing: 1px;
-        width: 5rem;
-        line-height: -20px;
-        text-transform: uppercase;
-
-        &:hover{
-        border-bottom: 2px solid #292929;
-        }
-}
-`
+  }
+`;
 
 const UserContainer = styled.div`
-   width: 55%;
-   height: 13rem;
-   border-left: 1px solid #c9c9c9;
+  width: 55%;
+  height: 13rem;
+  border-left: 1px solid #c9c9c9;
 
-@media screen and (max-width: 768px) {
-        display: none;
-    } 
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 
-
-h3{
+  h3 {
     margin-left: 2rem;
-}
-`
-
+  }
+`;
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,116 +1,110 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-function Login({ toggleLogIn ,setToggleLogIn }) {
+function Login({ toggleLogIn, setToggleLogIn, isLoggedIn, setIsLoggedIn }) {
+  return (
+    <FormContainer>
+      <Form>
+        <FormInputContainer>
+          <InputCont>
+            <input type="text" name="username" id="" placeholder="användarnamn" />
+          </InputCont>
+          <InputCont>
+            <input type="password" name="password" id="" placeholder="lösenord" />
+          </InputCont>
+        </FormInputContainer>
+      </Form>
 
-
-    return (
-            <FormContainer>
-                <Form>
-                    <FormInputContainer>
-                        <InputCont>
-                        <input type="text" name="username" id="" placeholder="användarnamn"/>
-                        </InputCont>
-                        <InputCont>
-                        <input type="password" name="password" id="" placeholder="lösenord"/>
-                        </InputCont>
-                    </FormInputContainer>
-                </Form>
-
-                <RegisterContainer>
-                    <div>
-                    <p>Inte medlem?</p>
-                    <a href="#" onClick={() => setToggleLogIn(!toggleLogIn)}>Registrera</a>
-                    </div>
-                    <LoginButton type="submit">logga in</LoginButton>
-                </RegisterContainer>
-
-            </FormContainer>
-    )
+      <RegisterContainer>
+        <div>
+          <p>Inte medlem?</p>
+          <a href="#" onClick={() => setToggleLogIn(!toggleLogIn)}>
+            Registrera
+          </a>
+        </div>
+        <LoginButton type="submit" onClick={() => setIsLoggedIn(!isLoggedIn)}>
+          logga in
+        </LoginButton>
+      </RegisterContainer>
+    </FormContainer>
+  );
 }
 
-export default Login
-
-
+export default Login;
 
 // Styleing -----------------------------------
 
-const FormContainer = styled.div `
-    display: flex; 
-    flex-direction: column;
-    margin-left: 2rem;
-    max-width: 19rem;
-    
-`
+const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 2rem;
+  max-width: 19rem;
+`;
 
-const Form = styled.form `
-    position: relative;
-    display: flex; 
-    flex-direction: row;
+const Form = styled.form`
+  position: relative;
+  display: flex;
+  flex-direction: row;
 
-    input {
-        padding: 5px;
-        border: 1px solid #000;
-        margin-right: 7px;
-    }
-`
+  input {
+    padding: 5px;
+    border: 1px solid #000;
+    margin-right: 7px;
+  }
+`;
 
-const FormInputContainer = styled.div `
-    display: flex;
-    margin-bottom: 10px;
-
-
-`
+const FormInputContainer = styled.div`
+  display: flex;
+  margin-bottom: 10px;
+`;
 
 const InputCont = styled.div`
-display: flex;
-flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 
-const LoginButton = styled.button `
-    height: 45px;
-    width: 150px;
+const LoginButton = styled.button`
+  height: 45px;
+  width: 150px;
+  font-family: 'Libre Franklin', sans-serif;
+  border: 2px solid #292929;
+  text-align: center;
+  background-color: transparent;
+  color: #292929;
+  font-weight: 400;
+  font-size: 16px;
+  text-transform: uppercase;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #292929;
+    color: whitesmoke;
+  }
+`;
+
+const RegisterContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  p {
     font-family: 'Libre Franklin', sans-serif;
-    border: 2px solid #292929;
-    text-align: center;
-    background-color: transparent;
-    color: #292929;
-    font-weight: 400;
-    font-size: 16px;
-    text-transform: uppercase;
-    cursor: pointer;
+    font-size: 0.8rem;
+    margin-right: 6px;
+  }
 
-    &:hover{
-        background-color: #292929;
-        color: whitesmoke;
+  a {
+    font-size: 0.8rem;
+    color: #4e5da3;
+    font-family: 'Montserrat';
+    text-decoration: none;
+    letter-spacing: 1px;
+    margin-bottom: 13px;
+
+    &:hover {
+      border-bottom: 1px solid #4e5da3;
     }
-`
-
-const RegisterContainer = styled.div `
+  }
+  div {
     display: flex;
-    flex-direction: column;
-
-    p{
-        font-family: 'Libre Franklin', sans-serif;
-        font-size: .8rem;
-        margin-right: 6px;
-    }
-
-    a {
-        font-size: .8rem;
-        color: #4e5da3;
-        font-family: 'Montserrat'; 
-        text-decoration: none;
-        letter-spacing: 1px;
-        margin-bottom: 13px;
-
-        &:hover{
-            border-bottom: 1px solid #4e5da3;
-    }
-    }
-   div{
-       display: flex;
-   }
-`
-
-const RegisterButton = styled(LoginButton)``
+  }
+`;
