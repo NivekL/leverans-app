a7a81688c8949a2d01f6c34143444ee9f8e11185
brorminskij <alexander.borminskij@gmail.com>
2021-09-20 15:41:52 +0200

    test map added

---
+++ test/test.js
@@ -0,0 +1,15 @@
+const {assert} = require('chai');
+
+// UNIT TESTING FÖR CART
+
+describe('Cart', () => {
+
+    describe('', () => {
+
+        it('', () => {
+            assert.throw( () => {
+
+                })
+            })
+        })
+    })
