52a64575397dde07752f33ff1869cd36e044b4e6
sofiabjorkman <70326065+sofiabjorkman@users.noreply.github.com>
2021-09-21 11:09:49 +0200

    login fixes
    Co-authored-by: Kevin Leskinen <kevnl_95@hotmail.com>
    Co-authored-by: Bryan Larsson <TheRealBrallis@users.noreply.github.com>

--- backend/index.js
+++ backend/index.js
@@ -115,14 +115,7 @@ app.get('/api/registration/:user_name/:password', async (req, res) => {
     let validPassword = await bcryptjs.compare(passwordDecoded, user[0].password);
   
     return res.json({ loginSuccess: validPassword, user_name: user[0].user_name });
-
-    //     } else {
-    //       res.status(400).json({ error: "Invalid Password" });
-    //     }
-    //   } else {
-    //     res.status(401).json({ error: "User does not exist" });
-    //   }
-    });
+});
 
 // ......BCRYPT
 
